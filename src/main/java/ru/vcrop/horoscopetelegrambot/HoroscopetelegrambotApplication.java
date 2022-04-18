a79f704cd1be9382f5a33b059e2cfae499e5c872
vcrop <viktor.raguev@gmail.com>
2022-04-18 15:01:20 +0300

    initial commit

---
+++ src/main/java/ru/vcrop/horoscopetelegrambot/HoroscopetelegrambotApplication.java
@@ -0,0 +1,13 @@
+package ru.vcrop.horoscopetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoroscopetelegrambotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoroscopetelegrambotApplication.class, args);
+    }
+
+}
